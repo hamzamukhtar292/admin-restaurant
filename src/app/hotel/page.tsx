8e58aed5c0352e50411d656860696b066040b305
Tiyaab Ali <tiyaabali@gmail.com>
2024-08-25 16:24:31 +0500

    signup page deign complete

--- src/app/hotel/page.tsx
+++ src/app/hotel/page.tsx
@@ -71,142 +71,144 @@ const CreateRestaurant: FC = () => {
         employees: 1,
       }}
       validationSchema={CreateRestaurantSchema}
-      onSubmit={(values:any) => {
+      onSubmit={(values: any) => {
         // Handle form submission
         //alert(location);
       }}
     >
-      {({ values, setFieldValue, errors, touched }:any) => (
+      {({ values, setFieldValue, errors, touched }: any) => (
         <div className="flex justify-center items-center  bg-base">
-      <form
-  onSubmit={handlecreate}
-  className="w-96 p-8 shadow-lg rounded-lg bg-base2 text-text border border-borderBase"
->
-  <h2 className="text-3xl font-bold mb-6 text-textMain text-center">
-    Create Restaurant
-  </h2>
-  
-  {/* Restaurant Name */}
-  <div>
-    <label htmlFor="restaurantName" className="text-textMain mb-2 block">
-      Restaurant Name
-    </label>
-    <Field
-      id="restaurantName"
-      name="restaurantName"
-      placeholder="Name"
-      className="w-full mb-4 p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-400 border-none bg-inputMain"
-    />
-    <ErrorMessage
-      name="restaurantName"
-      component="div"
-      className="text-red-500 mb-3"
-    />
-  </div>
-  
-  {/* Location */}
-  <div>
-    <label htmlFor="location" className="text-textMain mb-2 block">
-      Location
-    </label>
-    <Field
-      id="location"
-      name="location"
-      placeholder="Location"
-      className="w-full mb-4 p-3 border-none rounded-md focus:outline-none focus:ring-2 focus:ring-red-400 bg-inputMain"
-    />
-    <ErrorMessage
-      name="location"
-      component="div"
-      className="text-red-500 mb-3"
-    />
-  </div>
-  
-  {/* Owner */}
-  <div>
-    <label htmlFor="owner" className="text-textMain mb-2 block">
-      Owner
-    </label>
-    <Field
-      id="owner"
-      name="owner"
-      placeholder="Owner"
-      className="w-full mb-4 p-3 border-none rounded-md focus:outline-none focus:ring-2 focus:ring-red-400 bg-inputMain"
-    />
-    <ErrorMessage
-      name="owner"
-      component="div"
-      className="text-red-500 mb-3"
-    />
-  </div>
-  
-  {/* Employees */}
-  <div>
-    <label htmlFor="employees" className="text-textMain mb-2 block">
-      Employees: {values.employees}
-    </label>
-    <Field
-      id="employees"
-      name="employees"
-      type="range"
-      min="0"
-      max="100"
-      step="1"
-      value={values.employees}
-      className="w-full mb-4 p-3 border-none bg-base2 rounded-md"
-    />
-    <ErrorMessage
-      name="employees"
-      component="div"
-      className="text-red-500 mb-3"
-    />
-  </div>
-  
-  {/* Image Upload */}
-  <div>
-    <label className="text-textMain mb-2 block">Upload Images</label>
-    <div
-      {...getRootProps()}
-      className="border-2 border-dashed border-borderBase p-5 text-center cursor-pointer"
-    >
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p className="text-textMain">Drop the files here ...</p>
-      ) : (
-        <p className="text-textMain">
-          Drag and drop some files here, or click to select files
-        </p>
-      )}
-    </div>
-    <div className="flex flex-wrap mt-5">
-      {images.map((image, index) => (
-        <div key={index} className="m-2 relative">
-          <Image
-            src={image.preview}
-            alt={`img-${index}`}
-            width="100"
-            height="100"
-            className="w-[137px] h-[137px] object-cover border-2 border-red-600"
-          />
-          <button
-            onClick={() => handleImageDelete(index)}
-            className="absolute top-2 right-2 bg-grey6 text-textMain rounded-md w-6 h-6 flex items-center justify-center text-xs cursor-pointer"
+          <form
+            onSubmit={handlecreate}
+            className="w-96 p-8 shadow-lg rounded-lg bg-base2 text-text border border-borderBase"
           >
-            ✖
-          </button>
-        </div>
-      ))}
-    </div>
-  </div>
-  
-  <button
-    type="submit"
-    className="w-full p-3 mt-3 bg-grey6 text-textMain rounded hover:bg-grey7 transition"
-  >
-    Create
-  </button>
-</form>
+            <h2 className="text-3xl font-bold mb-6 text-textMain text-center">
+              Create Restaurant
+            </h2>
 
+            {/* Restaurant Name */}
+            <div>
+              <label
+                htmlFor="restaurantName"
+                className="text-textMain mb-2 block"
+              >
+                Restaurant Name
+              </label>
+              <Field
+                id="restaurantName"
+                name="restaurantName"
+                placeholder="Name"
+                className="w-full mb-4 p-3 border rounded-md focus:outline-none focus:ring-2 focus:ring-red-400 border-none bg-inputMain"
+              />
+              <ErrorMessage
+                name="restaurantName"
+                component="div"
+                className="text-red-500 mb-3"
+              />
+            </div>
+
+            {/* Location */}
+            <div>
+              <label htmlFor="location" className="text-textMain mb-2 block">
+                Location
+              </label>
+              <Field
+                id="location"
+                name="location"
+                placeholder="Location"
+                className="w-full mb-4 p-3 border-none rounded-md focus:outline-none focus:ring-2 focus:ring-red-400 bg-inputMain"
+              />
+              <ErrorMessage
+                name="location"
+                component="div"
+                className="text-red-500 mb-3"
+              />
+            </div>
+
+            {/* Owner */}
+            <div>
+              <label htmlFor="owner" className="text-textMain mb-2 block">
+                Owner
+              </label>
+              <Field
+                id="owner"
+                name="owner"
+                placeholder="Owner"
+                className="w-full mb-4 p-3 border-none rounded-md focus:outline-none focus:ring-2 focus:ring-red-400 bg-inputMain"
+              />
+              <ErrorMessage
+                name="owner"
+                component="div"
+                className="text-red-500 mb-3"
+              />
+            </div>
+
+            {/* Employees */}
+            <div>
+              <label htmlFor="employees" className="text-textMain mb-2 block">
+                Employees: {values.employees}
+              </label>
+              <Field
+                id="employees"
+                name="employees"
+                type="range"
+                min="0"
+                max="100"
+                step="1"
+                value={values.employees}
+                className="w-full mb-4 p-3 border-none bg-base2 rounded-md"
+              />
+              <ErrorMessage
+                name="employees"
+                component="div"
+                className="text-red-500 mb-3"
+              />
+            </div>
+
+            {/* Image Upload */}
+            <div>
+              <label className="text-textMain mb-2 block">Upload Images</label>
+              <div
+                {...getRootProps()}
+                className="border-2 border-dashed border-borderBase p-5 text-center cursor-pointer"
+              >
+                <input {...getInputProps()} />
+                {isDragActive ? (
+                  <p className="text-textMain">Drop the files here ...</p>
+                ) : (
+                  <p className="text-textMain">
+                    Drag and drop some files here, or click to select files
+                  </p>
+                )}
+              </div>
+              <div className="flex flex-wrap mt-5">
+                {images.map((image, index) => (
+                  <div key={index} className="m-2 relative">
+                    <Image
+                      src={image.preview}
+                      alt={`img-${index}`}
+                      width="100"
+                      height="100"
+                      className="w-[137px] h-[137px] object-cover border-2 border-red-600"
+                    />
+                    <button
+                      onClick={() => handleImageDelete(index)}
+                      className="absolute top-2 right-2 bg-grey6 text-textMain rounded-md w-6 h-6 flex items-center justify-center text-xs cursor-pointer"
+                    >
+                      ✖
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full p-3 mt-3 bg-grey6 text-textMain rounded hover:bg-grey7 transition"
+            >
+              Create
+            </button>
+          </form>
         </div>
       )}
     </Formik>
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,31 @@
-'use client';
+<p>Ya sara Code Tayyab Ali na Comment kiya howaS</p>
+/*"use client";
 
-import React, { useState } from 'react';
-import { useRouter } from 'next/router';
+import React, { useState } from "react";
+import { useRouter } from "next/router";
 
 const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  //const router = useRouter();
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
     // Dummy authentication logic
-    if (username === 'admin' && password === 'password') {
-      localStorage.setItem('isAuthenticated', 'true');
-      router.push('/dashboard');
+    if (username === "admin" && password === "password") {
+      localStorage.setItem("isAuthenticated", "true");
+      router.push("/dashboard");
     } else {
-      alert('Invalid credentials');
+      alert("Invalid credentials");
     }
   };
 
   return (
     <div className="flex justify-center items-center h-screen bg-gradient-to-r from-yellow-100 via-red-100 to-yellow-100 dark:from-gray-800 dark:via-gray-900 dark:to-gray-800">
-      <form onSubmit={handleLogin} className="w-96 p-8 shadow-lg rounded-lg bg-white text-black dark:bg-gray-700 dark:text-white">
+      <form
+        onSubmit={handleLogin}
+        className="w-96 p-8 shadow-lg rounded-lg bg-white text-black dark:bg-gray-700 dark:text-white"
+      >
         <h2 className="text-3xl font-bold mb-6 text-center text-red-600 dark:text-red-300">
           Admin Restaurant Login
         </h2>
@@ -39,7 +43,10 @@ const Login = () => {
           onChange={(e) => setPassword(e.target.value)}
           className="w-full mb-6 p-3 border rounded focus:outline-none focus:ring-2 focus:ring-red-400 dark:bg-gray-600 dark:border-gray-500"
         />
-        <button type="submit" className="w-full p-3 bg-red-600 text-white rounded hover:bg-red-700 transition dark:bg-red-500 dark:hover:bg-red-400">
+        <button
+          type="submit"
+          className="w-full p-3 bg-red-600 text-white rounded hover:bg-red-700 transition dark:bg-red-500 dark:hover:bg-red-400"
+        >
           Login
         </button>
       </form>
@@ -48,3 +55,4 @@ const Login = () => {
 };
 
 export default Login;
+*/
